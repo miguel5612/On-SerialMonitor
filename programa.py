bd5c84461549f347f1cb44c68403c11e856b1d66
unknown <miguelangelcu@ufps.edu.co>
2019-02-28 15:29:40 -0500

    Se realiza el programa que guardara los datos

---
+++ programa.py
@@ -0,0 +1,47 @@
+
+import sys, os, serial, threading
+from datetime import datetime
+
+now = datetime.now()
+
+def monitor():
+
+    ser = serial.Serial(COMPORT, BAUDRATE, timeout=0)
+    
+    printFile("SEP = ," + CARRIAGE_RETURN)
+
+    while (1):
+        line = readData(ser)
+        print(line)
+        printFile(line + CARRIAGE_RETURN)
+
+    print("Stop Monitoring")
+
+
+
+def readData(ser):
+    buffer = ""
+    while True:
+        oneByte = ser.read(1)
+        if oneByte == b"\r":    #method should returns bytes
+            return buffer.split(":")[1].strip()
+        else:
+            buffer += oneByte.decode("ascii")
+
+def printFile(line):
+    text_file = open(FOLDER + FILENAME, "a")
+    text_file.write(line)
+    text_file.close()
+
+print("Start Serial Monitor")
+
+COMPORT = "COM5"
+BAUDRATE = 115200
+EXTENSION = "csv"
+CARRIAGE_RETURN = "\r"
+FILENAME = str(now.strftime("%Y_%m_%d_%H_%M") + "_log_" + "bogohack" + "." + EXTENSION)
+FOLDER = "Resultados/"
+print(FILENAME)
+
+monitor()
+			
