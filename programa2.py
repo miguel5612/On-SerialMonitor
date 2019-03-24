6c8c58c5c6a20e1eb80f0c98eb4de7da4502bfab
unknown <miguelangelcu@ufps.edu.co>
2019-03-23 20:51:21 -0500

    Clase 2

--- programa2.py
+++ programa2.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 now = datetime.now()
 headers = 'Hotbed, Extruder, Motor X, Motor Y, Motor Z1, Motor Z2, Voltaje, Corriente, Potencia electrica (W), Potencia electrica(KW/h), Objeto 3D (Descripcion)'
-NombreImpresion = "Clase impresion 3D Vivelab"
+NombreImpresion = "prueba_texto_Vivelab"
 
 def monitor():
 
